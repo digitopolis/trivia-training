d77fa9856d89d50b42f6ce117af81b7c6711d6f2
Matt Readout <matt.readout@gmail.com>
2020-10-29 11:00:33 -0700

    Import question JSON data, shuffle and take 10

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Intro from "./components/intro";
+import { get10Questions, shuffleQuestions } from "./utilities/quizHelpers";
+
+const DATA = require("./data.json");
 
 function App() {
+  const [questions, setQuestions] = useState(
+    get10Questions(shuffleQuestions(DATA))
+  );
+
   return (
     <div className="App">
       <BrowserRouter>
---
+++ src/utilities/quizHelpers.tsx
@@ -0,0 +1,20 @@
+interface Question {
+  question: string;
+  incorrect: string[];
+  correct: string;
+}
+
+export const shuffleQuestions = (questionData: Question[]): Question[] => {
+  const shuffledArray: Question[] = [...questionData];
+  for (let index: number = shuffledArray.length - 1; index >= 0; index--) {
+    const swap: number = Math.floor(Math.random() * index);
+    const temp = shuffledArray[swap];
+    shuffledArray[swap] = shuffledArray[index];
+    shuffledArray[index] = temp;
+  }
+  return shuffledArray;
+};
+
+export const get10Questions = (questionData: Question[]): Question[] => {
+  return questionData.slice(0, 10);
+};
